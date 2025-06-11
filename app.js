9c731a18c5746e9cb8310ab81d39ab405189bb87
ayushudaydubey <102001ayushdubey@gmail.com>
2025-06-11 15:38:11 +0530

    remove option

--- app.js
+++ app.js
@@ -16,12 +16,7 @@ import userModel from './src/Models/users.models.js'
 const app = express()
 const server = createServer(app)
 
-app.options('*', cors({
-  origin: ['http://localhost:5173','https://chatzone-frontend.vercel.app'],
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'Cookie']
-}))
+
 
 // Initialize Socket.IO FIRST
 const io = new Server(server, {
